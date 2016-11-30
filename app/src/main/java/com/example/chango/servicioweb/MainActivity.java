fae533242a02508ca9765e936124d0a719e04910
alumnoIdat <leomanarael@gmail.com>
2016-11-30 09:30:51 -0500

    finished

--- app/src/main/java/com/example/chango/servicioweb/MainActivity.java
+++ app/src/main/java/com/example/chango/servicioweb/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.chango.servicioweb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText usernameField,passwordField;
+    private TextView status,role,method;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usernameField = (EditText)findViewById(R.id.editText);
+        passwordField = (EditText)findViewById(R.id.editText2);
+        status = (TextView)findViewById(R.id.textView7);
+        role = (TextView)findViewById(R.id.textView9);
+        method = (TextView)findViewById(R.id.textView5);
     }
+
+    public void login(View view){
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        method.setText("Get Metodo");
+        new SigninActivity(this,status,role,0).execute(username,password);
+    }
+
+    public void loginPost(View view){
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        method.setText("Post Metodo");
+        new JSON(this,status,role,1).execute(username,password);
+    }
+
 }
